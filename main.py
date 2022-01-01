c38ed0a7ae734d9cff27c81fcb17869e3cac3c7f
17122981 <44666980+17122981@users.noreply.github.com>
2022-01-01 16:57:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import base64
 import easyocr
 import io
 import numpy
+import requests
 from PIL import Image
 from PIL import ImageEnhance
 
@@ -170,23 +171,13 @@ class Zlapp(Fudan):
         print(type(io.BytesIO(img_byte)))
         print(io.BytesIO(img_byte))
         
-        img = Image.open(io.BytesIO(img_byte)).convert('L')
-        enh_bri = ImageEnhance.Brightness(img)
-        new_img = enh_bri.enhance(factor=1.5)
-        
-        image = numpy.array(new_img)
-        reader = easyocr.Reader(['en'])
-        horizontal_list, free_list = reader.detect(image, optimal_num_chars=4)
-        character = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        allow_list = list(character)
-        allow_list.extend(list(character.lower()))
-    
-        result = reader.recognize(image, 
-                                allowlist=allow_list,
-                                horizontal_list=horizontal_list[0],
-                                free_list=free_list[0],
-                                detail = 0)
-        return result[0]
+        data = {"username": uname, "password": pwd, "typeid": 3, "image": img_byte}
+        result = json.loads(requests.post("http://api.ttshitu.com/predict", json=data).text)
+        if result['success']:
+            return result["data"]["result"]
+        else:
+            return result["message"]
+        return ""
     
 
     def validate_code(self):
